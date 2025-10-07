262c240b8d82ac9354a3491a583c133a64193ded
Vishesh Rajput <vishesh.rajput.p@gmail.com>
2025-10-07 19:23:53 +0000

    added MUI lib and input fields in form

--- react-cv-genrator/src/App.jsx
+++ react-cv-genrator/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import Form from './components/Form';
 import Preview from './components/Preview';
+import { Typography,Button } from '@mui/material';
 
 function App() {
 
     const [formData ,setFormData] = useState({name:''});
+    const [backColor , setBackColor] = useState("red");
 
+    function setcolor(){
+      setBackColor('green');
+    }
+var col = "blue";
   return (
     <>
-   <h1>CV genrator</h1>
-    <Form formData = {formData} setFormData = {setFormData}/>
-    <Preview/>
+    <Form/>
+    <Typography variant='h2' sx={{color : "blue", fontSize:"50px", fontWeight:" 700" }}>
+
+Vishesh is great web devloper
+    </Typography>
+    
+<Button variant='contained' href='https://google.com'>smash</Button>
+<Button variant='text' sx={{backgroundColor: backColor}} onClick={setcolor}>smash </Button>
+<Button variant='outlined' sx={{color: "red" , backgroundColor: " blue" , pl:"4", m:'40px'
+  
+}}>smash</Button>
     </>
   )
 }
--- react-cv-genrator/src/components/Form.jsx
+++ react-cv-genrator/src/components/Form.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+import { TextField , Button } from "@mui/material";
 
 
 
 function Form(){
+
+    const [inputs , setInputs] = useState({
+        name:'',
+        email:"",
+        college:''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputs((prevInputs) => ({
+            ...prevInputs,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(inputs);
+        
+        
+    }
     return(
     <>
-    <input type="text" value={FormData.name} />
-
-    
+    {/* <input type="text" value={FormData.name} /> */}
+    < form onSubmit={handleSubmit}>
+<TextField autoComplete="on" name='name' type="text"   helperText="Please enter your name" value={inputs.name} onChange={handleChange}></TextField>
+<TextField autoComplete="on" name="email" type="email"  sx={{m:" 0 100px "}} placeholder="Enter email" value={inputs.email} onChange={handleChange}></TextField>
+<TextField autoComplete="on" name="college" type="text" placeholder="Enter college" value={inputs.college} onChange={handleChange}></TextField>
+    <Button type="submit" variant="contained" >submit</Button>
+    </form >
     </>
 
 
